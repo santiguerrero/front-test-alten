990c51cfb5056dc0802c88124af7dab95a193671
santiguerrero <santilgunvo@gmail.com>
2024-02-14 16:44:45 +0100

    adding angular material / bootstrap, inizio for custom theming

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const CurrentModules: string[] = [
+
+];
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: []
+})
+export class MaterialModule { }
